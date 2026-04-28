cbd904dd130f93b02680318a53204fd581ffc0ce
edward <edwardlfish@gmail.com>
2026-04-28 18:15:18 +0300

    Replace incorrect app import with express and disable timestamps in Sequelize config.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import {dbConnection} from "./config/database.js";
 import dotenv from 'dotenv';
-import app from "express/lib/application.js";
+import express from 'express';
 import {syncModels} from "./model/index.js";
 
 dotenv.config();
+const app = express();
 
 const startServer = async () => {
     await dbConnection();
