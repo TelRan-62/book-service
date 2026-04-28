856d2b378e95ec39229cd9490ff6ba1878442d82
edward <edwardlfish@gmail.com>
2026-04-28 20:20:07 +0300

    Add book retrieval by ISBN API and route

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -40,3 +40,21 @@ export const addBook = async (req, res) => {
         });
     }
 }
+
+export const findBookByIsbn = async (req, res) => {
+    const book = await Book.findByPk(req.params.isbn);
+    if (book) {
+        const result = {
+            isbn: book.isbn,
+            title: book.title,
+            publisher: book.publisher,
+            authors: (await book.getAuthors()).map(a => ({
+                name: a.name,
+                birthDate: a.birth_date
+            }))
+        }
+        return res.json(result);
+    } else {
+        return res.status(404).send({error: `Book with ISBN ${req.params.isbn} not found`});
+    }
+}
