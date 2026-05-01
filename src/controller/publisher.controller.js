d38ff9753a8e0c89fc1ff8f19550f1260c189c29
edward <edwardlfish@gmail.com>
2026-05-01 11:38:07 +0300

    Implement publishers retrieval by author API

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,3 +1,11 @@
+import {Author} from "../model/index.js";
+
 export const findPublishersByAuthor = async (req, res) => {
-    // TODO: Implement publishers search by author logic
+    const author = await Author.findByPk(req.params.name);
+    if (!author) {
+        return res.status(404).json({error: `Author with name ${req.params.name} not found`});
+    }
+    const books = await author.getBooks();
+    const publishers = [...new Set(books.map(book => book.publisher))];
+    return res.json(publishers);
 }
