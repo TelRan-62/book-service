163a22fba56d49425db26a9f16d8406e7fb0d76e
edward <edwardlfish@gmail.com>
2026-05-01 10:41:24 +0300

    Implement updateBookTitle and book search by author/publisher with transaction handling

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -103,13 +103,90 @@ export const removeBook = async (req, res) => {
 }
 
 export const updateBookTitle = async (req, res) => {
-    // TODO: Implement book title update logic
+    const t = await sequelize.transaction();
+    try {
+        const book = await Book.findByPk(req.params.isbn,
+            {
+                include: [
+                    {
+                        model: Author,
+                        as: 'authors',
+                        attributes: {
+                            include: ['name', [sequelize.col('birth_date'), 'birthDate']],
+                            exclude: ['birth_date']
+                        },
+                        through: {
+                            attributes: []
+                        }
+                    }
+                ],
+                transaction: t
+            });
+        if (book) {
+            // book.title = req.params.title;
+            // await book.save({transaction: t});
+            await book.update({title: req.params.title}, {transaction: t});
+            await t.commit();
+            return res.json(book);
+        } else {
+            await t.rollback();
+            return res.status(404).send({error: `Book with ISBN ${req.params.isbn} not found`});
+        }
+    } catch (e) {
+        await t.rollback();
+        console.log(`Error removing book:`, e);
+        return res.status(500).send({
+            error: e.message,
+            message: 'Failed to remove book'
+        });
+    }
 }
 
 export const findBooksByAuthor = async (req, res) => {
-    // TODO: Implement books search by author logic
+    const author = await Author.findByPk(req.params.name);
+    if (!author) {
+        return res.status(404).send({error: `Author with name ${req.params.name} not found`});
+    }
+    const books = await author.getBooks({
+        include: [
+            {
+                model: Author,
+                as: 'authors',
+                attributes: {
+                    include: ['name', [sequelize.col('birth_date'), 'birthDate']],
+                    exclude: ['birth_date']
+                },
+                through: {
+                    attributes: []
+                }
+            }
+        ],
+        joinTableAttributes: []
+    });
+    return res.json(books);
 }
 
 export const findBooksByPublisher = async (req, res) => {
-    // TODO: Implement books search by publisher logic
+    if(!await Publisher.findByPk(req.params.name)) {
+        return res.status(404).send({error: `Publisher with name ${req.params.name} not found`});
+    }
+    const books = await Book.findAll({
+        where: {
+            publisher: req.params.name
+        },
+        include: [
+            {
+                model: Author,
+                as: 'authors',
+                attributes: {
+                    include: ['name', [sequelize.col('birth_date'), 'birthDate']],
+                    exclude: ['birth_date']
+                },
+                through: {
+                    attributes: []
+                }
+            }
+        ]
+    });
+    return res.json(books);
 }
